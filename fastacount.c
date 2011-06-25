5e034f6288d837163d734b6147f47a54919f6d6c
greg <greg@gblaptop>
2011-06-25 10:02:21 -0700

    Added GPLv3 license

--- fastacount.c
+++ fastacount.c
@@ -1,9 +1,24 @@
 /*
- * GCB 4jun11
- *
  * Report length of FASTA sequences, excluding gaps.
  * normal mode - report only the length of the first sequence in the input.
  * verbose mode - report length of all sequences and echo comments
+ *
+ * Copyright (C) 2011, Greg Benison
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  */
 
 #include <stdio.h>
--- fastatail.c
+++ fastatail.c
@@ -1,7 +1,22 @@
 /*
- * GCB 5jun11
- *
  * Echo FASTA input sequences, including all but first 'n' bases.
+ *
+ * Copyright (C) 2011, Greg Benison
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  */
 
 #include <stdio.h>
